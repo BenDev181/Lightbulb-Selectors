8be9a55367a8ff8f1fbe20786f400ca9d6bb8769
BenDev181 <benjiro181@gmail.com>
2024-09-26 11:14:31 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Write your code here
+let bulb1 = document.querySelector("#lightbulb1");
+let bulb2 = document.querySelector("#lightbulb2");
+let bulb3 = document.querySelector("#lightbulb3");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+bulb1.addEventListener("click", function() {
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    bulb1.classList.toggle("active");
+})
+
+bulb2.addEventListener("click", function() {
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    bulb2.classList.toggle("active");
+})
+
+bulb3.addEventListener("click", function() {
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    bulb3.classList.toggle("active");
+})
